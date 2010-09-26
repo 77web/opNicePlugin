4cfcd7e46af532c5d81ef55dd4f050d22f32dc61
77web <info@77-web.com>
2010-09-26 09:33:16 +0900

    regist/delete action

--- lib/action/opNicePluginNiceActions.class.php
+++ lib/action/opNicePluginNiceActions.class.php
@@ -4,10 +4,27 @@ class opNicePluginNiceActions extends sfActions
 {
   public function executeRegist(sfWebRequest $request)
   {
+    $this->form = new NiceForm();
+    $this->form->bind($request->getParameter($this->form->getName()));
+    if($this->form->isValid())
+    {
+      $nice = $this->form->save();
+    }
+    $this->redirect($nice->getForeignUrl());
   }
   
   public function executeDelete(sfWebRequest $request)
   {
+    $this->nice = Doctrine::getTable('Nice')->find($request->getParameter('id', 0));
+    $this->forward404Unless($this->nice && $this->nice->isDeletable($this->getUser()->getMemberId()));
+    
+    $request->checkCSRFProtection();
+    
+    $url = $this->nice->getForeignUrl();
+    
+    $this->nice->delete();
+    
+    $this->redirect($url);
   }
   
 }
