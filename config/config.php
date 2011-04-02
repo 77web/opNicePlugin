049e03c894ba8d3507009bbdddec96c0d09e09e3
77web <info@77-web.com>
2011-04-02 16:30:10 +0900

    change plugin config file. remove config.php and add configuration class.

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//remover
-$this->dispatcher->connect('op_action.post_execute_diary_delete', array("opNicePluginNiceRemover", "listenToDiaryDelete"));
-$this->dispatcher->connect('op_action.post_execute_communityEvent_delete', array("opNicePluginNiceRemover", "listenToCommunityEventDelete"));
-$this->dispatcher->connect('op_action.post_execute_communityTopic_delete', array("opNicePluginNiceRemover", "listenToCommunityTopicDelete"));
---
+++ config/opNicePluginConfiguration.class.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class opNicePluginConfiguration extends sfPluginConfiguration
+{
+  public function initialize()
+  {
+    $this->dispatcher->connect('op_action.post_execute_diary_delete', array("opNicePluginNiceRemover", "listenToDiaryDelete"));
+    
+    $this->dispatcher->connect('op_action.post_execute_communityEvent_delete', array("opNicePluginNiceRemover", "listenToCommunityEventDelete"));
+    
+    $this->dispatcher->connect('op_action.post_execute_communityTopic_delete', array("opNicePluginNiceRemover", "listenToCommunityTopicDelete"));
+  }
+
+}
